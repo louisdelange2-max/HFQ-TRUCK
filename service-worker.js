e82d3a747eb86b343aeaa9a7f1914af0d798cbdb
louisdelange2-max <louisdelange2@gmail.com>
2026-05-14 07:16:40 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // HFQ Truck Onboarding Service Worker
 // Version: 1.0.26
-const CACHE='hfq-truck-onboarding-delete-unsynced-queue-v1.0.27';
+const CACHE='hfq-truck-onboarding-delete-unsynced-queue-v1.0.28';
 const ASSETS=['./','./index.html','./manifest.json','./service-worker.js','./icons/hfq-logo.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>null))) });
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('hfq-truck-onboarding-fixed-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
